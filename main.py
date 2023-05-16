32d73685ad83c7ec9d80865cfe553993a5d2f726
Houssam Mehdi <houssam_mahdi_98@hotmail.com>
2023-05-16 03:47:54 +0200

    margins, alignment, images, tables

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import A4, LETTER, LEGAL, ELEVENSEVENTEEN
+from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER, TA_JUSTIFY
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.platypus import Table, Image, TableStyle
 from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib import colors
 from io import BytesIO
 import base64
 import json
@@ -29,35 +31,88 @@ def convert_to_pdf_route():
     print("Handling convert-to-pdf request")
     return convert_to_pdf(request)
 
-def gcf_convert_to_pdf(request):
-    return app(request.environ, functions.response)
+def getPageSize(page_size_name):
+    page_sizes = {
+        'A4': A4,
+        'LETTER': LETTER,
+        'LEGAL': LEGAL,
+        'ELEVEN_SEVENTEEN': ELEVENSEVENTEEN
+    }
+
+    try:
+        return page_sizes[page_size_name.upper()]
+    except KeyError:
+        raise ValueError(f"Invalid page size: {page_size_name}")
 
 def convert_to_pdf(request):
     # Parse the JSON body of the request
     data = json.loads(request.data)
 
     # Extract parameters from the JSON object
-    text = data.get('text')
-    font_size = data.get('font_size', 12)
-    font_style = data.get('font_style', 'Normal')
+    paragraphs = data.get('paragraphs', [])
+    page_size = data.get('page_size', 'A4')
+    margins = data.get('margins', {'top': 72, 'bottom': 72, 'left': 72, 'right': 72})
 
-    # Check if the text is not empty
-    if not text:
-        return {"error": "No text provided for conversion"}
+    # Check if the paragraphs is not empty
+    if not paragraphs:
+        return {"error": "No paragraphs provided for conversion"}
 
-    # Check if the font style is valid
-    if font_style not in ['Normal', 'Italic', 'Heading1', 'Title', 'Heading2', 'Heading3', 'Bullet', 'Definition', 'Code']:
-        return {"error": "Invalid font style provided"}
+    # Check if the page size is valid
+    try:
+        page_size = getPageSize(page_size)
+    except ValueError as e:
+        return {"error": str(e)}
 
     # Create a BytesIO buffer for the PDF to be stored in
     buffer = BytesIO()
 
     # Create the PDF object, using the BytesIO object as its "file"
-    doc = SimpleDocTemplate(buffer, pagesize=A4)
+    doc = SimpleDocTemplate(buffer, pagesize=page_size, 
+                            topMargin=margins.get('top', 72), 
+                            bottomMargin=margins.get('bottom', 72), 
+                            leftMargin=margins.get('left', 72), 
+                            rightMargin=margins.get('right', 72))
 
-    # Create the list of Paragraph objects
-    styles = getSampleStyleSheet()
-    story = [Paragraph(text, styles[font_style])]
+    # Create the list of Flowable objects
+    story = []
+    alignment_dict = {'LEFT': TA_LEFT, 'RIGHT': TA_RIGHT, 'CENTER': TA_CENTER, 'JUSTIFY': TA_JUSTIFY}
+    for paragraph in paragraphs:
+        text = paragraph.get('text')
+        if text is None:
+            continue  # Skip this paragraph if text is None
+
+        font_size = paragraph.get('font_size', 12)
+        font_style = paragraph.get('font_style', 'Normal')
+        alignment = paragraph.get('alignment', 'LEFT')
+
+        style = ParagraphStyle(
+            name=font_style,
+            fontSize=font_size,
+            alignment=alignment_dict.get(alignment, TA_LEFT)
+        )
+
+        story.append(Paragraph(text, style))
+
+        # Add table if exists in paragraph
+        # Add table if exists in paragraph
+        if 'table' in paragraph:
+            table_data = paragraph['table'].get('data', [])
+            table_style_data = paragraph['table'].get('style', [])
+
+            # Create a TableStyle object from the style data
+            table_style = TableStyle([
+                (style, (0, i), (-1, i), colors.black)
+                for i, style in enumerate(table_style_data)
+            ])
+
+            story.append(Table(table_data, style=table_style))
+
+        # Add image if exists in paragraph
+        if 'image' in paragraph:
+            image_src = paragraph['image'].get('src', '')
+            image_width = paragraph['image'].get('width', 100)
+            image_height = paragraph['image'].get('height', 100)
+            story.append(Image(image_src, width=image_width, height=image_height))
 
     # Build the PDF
     doc.build(story)
